dcec4c14a27bdcaa3d37138fddff02686e02303a
Raphael Stefanini <raphael@rphlo.com>
2024-05-16 14:39:26 +0300

    Update command to something useful

--- project/routedb/management/commands/remove_unused_files.py
+++ project/routedb/management/commands/remove_unused_files.py
@@ -1,18 +1,21 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from utils.s3 import get_s3_client, s3_delete_key
-
+from routedb.models import RasterMap, UserSettings
 
 class Command(BaseCommand):
-    help = "Remove image file from prev version"
+    help = "Remove unused image file from storage"
 
-    def scan_map_directory(self):
+    def add_arguments(self, parser):
+        parser.add_argument("--force", action="store_true", default=False)
+
+    def scan_directory(self, directory):
         # Should use v2 but wasabi fails to list all files with it
         # paginator = s3.get_paginator('list_objects_v2')
         paginator = self.s3.get_paginator("list_objects")
         kwargs = {
             "Bucket": settings.AWS_S3_BUCKET,
-            "Prefix": "routes/",
+            "Prefix": directory,
         }
         for page in paginator.paginate(**kwargs):
             try:
@@ -22,9 +25,44 @@ class Command(BaseCommand):
             for obj in contents:
                 key = obj["Key"]
                 yield key
-
+    def process_image_file(self, image_name, force):
+        if image_name not in self.image_paths:
+            self.n_image_removed += 1
+            self.stdout.write(f"File {image_name} is unused")
+            if force:
+                s3_delete_key(image_name, settings.AWS_S3_BUCKET)
+        else:
+            self.stdout.write(f"File {image_name} is used")
+            self.n_image_keeped += 1
+    
     def handle(self, *args, **options):
+        force = options["force"]
+        self.image_paths = set(RasterMap.objects.values_list("image", flat=True))
+
+        self.image_paths.update(
+            set(
+                UserSettings.objects.all()
+                .exclude(avatar__isnull=True)
+                .exclude(avatar="")
+                .values_list("avatar", flat=True)
+            )
+        )
+        self.n_image_removed = 0
+        self.n_image_keeped = 0
         self.s3 = get_s3_client()
-        for filename in self.scan_map_directory():
-            s3_delete_key(filename, settings.AWS_S3_BUCKET)
-        self.stdout.write(self.style.SUCCESS("Done"))
+        for directory in ("maps", "avatars"):
+            for filename in self.scan_directory(directory):
+                self.process_image_file(filename, force)
+
+        if force:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Successfully removed {self.n_image_removed} files, "
+                    f"keeping {self.n_image_keeped}"
+                )
+            )
+        else:
+            self.stdout.write(
+                f"Would remove {self.n_image_removed} files, "
+                f"keeping {self.n_image_keeped}"
+            )
