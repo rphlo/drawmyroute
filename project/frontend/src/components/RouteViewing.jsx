069504b695ec523533f75ec961182bb225040e8c
Rapha S <rphlo@users.noreply.github.com>
2024-09-19 14:24:19 +0300

    Update RouteViewing.jsx

--- project/frontend/src/components/RouteViewing.jsx
+++ project/frontend/src/components/RouteViewing.jsx
@@ -405,7 +405,7 @@ const RouteViewing = (props) => {
           onPrivacyChanged={setIsPrivate}
         />
         <div className="mb-3">
-        {likes.length !== 0 && (<button type="button" dataTip={likers} dataEvent='click focus' className="font-weight-bold font-italic btn">{likes.length} 🏅</button>)}
+        {likes.length !== 0 && (<button type="button" data-tip={likers} data-event='click focus' className="font-weight-bold font-italic btn">{likes.length} 🏅</button>)}
         {canLike && (<> <button type="button" className="btn btn-primary" onClick={grantMedal}>Give a medal 🏅</button></>)}
         </div>
         {!cropping && (
