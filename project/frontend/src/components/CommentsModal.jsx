0eefc783836f9971c61f1370b2f3b82547f6efd9
Raphael Stefanini <raphael@rphlo.com>
2024-12-16 19:41:03 +0200

    Ship fast

---
+++ project/frontend/src/components/CommentsModal.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const CommentsModal = (props) => {
+
+  return (
+    <div
+      className="modal"
+      role="dialog"
+      style={{ display: "block", zIndex: 1e19 }}
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header" style={{ padding: "35px 50px" }}>
+            <h4>
+              <i className="fa fa-comment"></i> Comments
+            </h4>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              onClick={props.onClose}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body" style={{ padding: "40px 50px" }}>
+            {props.comments.length === 0 && (<div><b>No Comments</b><hr/></div>)}
+            {props.comments.map((comment) => {
+              <>
+                <p>{comment.message}</p>
+                <span>{comment.user.username === props.username ? "You" : (comment.user.first_name && comment.user.last_name ?
+        capitalizeFirstLetter(comment.user.first_name) +
+        " " +
+        capitalizeFirstLetter(comment.user.last_name)
+        : comment.user.username)}</span>
+                <hr/>
+              </>
+            })}
+            {props.canComment ? (<form onSubmit={props.onComment}>
+              <textarea className="form-control mb-3" name="message"></textarea>
+              <button className="btn btn-primary">Submit</button>
+            </form>) : (<div>Login to comment</div>)}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentsModal;
